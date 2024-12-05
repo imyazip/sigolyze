d74c343ba7218fd25c552c98ca9d5667b8945e0b
imyazip <tohchac@gmail.com>
2024-12-05 10:14:55 +0300

    Добавлена и протестирована функция LoadRules

--- sygolyze.go
+++ sygolyze.go
@@ -1,5 +1,9 @@
 package sigolyze
 
+import (
+	json "github.com/json-iterator/go"
+)
+
 type Pattern struct {
 	Name    string `json:"name"`
 	Value   string `json:"value"`
@@ -16,4 +20,44 @@ type Signature struct {
 	Patterns []Pattern  `json:"patterns"`
 	Tags     []string   `json:"tags"`
 	Meta     []MetaInfo `json:"meta"`
+}
+
+type Compiler struct {
+	Signatures Signature
+}
+
+func (p *Pattern) NewPattern(name string, value string, isRegex bool) *Pattern {
+	return &Pattern{
+		Name:    name,
+		Value:   value,
+		IsRegex: isRegex,
+	}
+}
+
+func (m *MetaInfo) NewMetaInfo(name string, info []string) *MetaInfo {
+	return &MetaInfo{
+		Name: name,
+		Info: info,
+	}
+}
+
+func (s *Signature) NewSignature(name string, patterns []Pattern, tags []string, meta []MetaInfo) *Signature {
+	return &Signature{
+		Name:     name,
+		Patterns: patterns,
+		Tags:     tags,
+		Meta:     meta,
+	}
+}
+
+func NewCompiler() *Compiler {
+	return &Compiler{}
+}
+
+func (c *Compiler) LoadRules(jsonString string) error {
+	err := json.Unmarshal([]byte(jsonString), &c.Signatures)
+	if err != nil {
+		return err
+	}
+	return nil
 }
---
+++ testing/loadrules_test.go
@@ -0,0 +1,87 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/imyazip/sigolyze"
+)
+
+var signs string = `
+{
+    "name": "Example Signature",
+    "patterns": [
+      {
+        "name": "Pattern 1",
+        "value": "Value1",
+        "is_regex": true
+      },
+      {
+        "name": "Pattern 2",
+        "value": "Value2",
+        "is_regex": false
+      }
+    ],
+    "tags": ["tag1", "tag2"],
+    "meta": [
+      {
+        "name": "Meta1",
+        "info": ["detail1", "detail2"]
+      },
+      {
+        "name": "Meta2",
+        "info": ["detail3"]
+      }
+    ]
+  }
+	`
+
+func TestLoadRules(t *testing.T) {
+	compiler := sigolyze.NewCompiler()
+	compiler.LoadRules(signs)
+	expectedPatterns := []sigolyze.Pattern{
+		{
+			Name:    "Pattern 1",
+			Value:   "Value1",
+			IsRegex: true,
+		},
+		{
+			Name:    "Pattern 2",
+			Value:   "Value2",
+			IsRegex: false,
+		},
+	}
+	expectedMeta := []sigolyze.MetaInfo{
+		{Name: "Meta1",
+			Info: []string{"detail1", "detail2"},
+		},
+		{Name: "Meta2",
+			Info: []string{"detail3"},
+		},
+	}
+
+	for index, _ := range expectedPatterns {
+		if expectedPatterns[index].Name != compiler.Signatures.Patterns[index].Name {
+			t.Errorf("Failed loading pattern names")
+		}
+
+		if expectedPatterns[index].Value != compiler.Signatures.Patterns[index].Value {
+			t.Errorf("Failed loading pattern values")
+		}
+
+		if expectedPatterns[index].IsRegex != compiler.Signatures.Patterns[index].IsRegex {
+			t.Errorf("Failed loading pattern values")
+		}
+	}
+
+	for index, _ := range expectedMeta {
+		if expectedMeta[index].Name != compiler.Signatures.Meta[index].Name {
+			t.Error("Failed loading metadata names")
+		}
+
+		for metaIndex, _ := range expectedMeta[index].Info {
+			if expectedMeta[index].Info[metaIndex] != compiler.Signatures.Meta[index].Info[metaIndex] {
+				t.Error("Failed loading metadata info")
+			}
+		}
+	}
+}
